da04565ce3e6c31ef871ff095d311fab7e0eb05c
jeff.wenzbauer <jwenz723@gmail.com>
2018-06-15 11:37:45 -0600

    Added deallocation of memory for Cstring

--- pdh/pdh.go
+++ pdh/pdh.go
@@ -3,8 +3,13 @@ package pdh
 // #cgo LDFLAGS: -L./lib -lpdh
 // #include <pdh.h>
 import "C"
+import (
+	"unsafe"
+)
 
 func PdhValidatePath(path string) uint32 {
-	ret := C.PdhValidatePath(C.CString(path))
+	cPath := C.CString(path)
+	ret := C.PdhValidatePath(cPath)
+	defer C.free(unsafe.Pointer(cPath))
 	return uint32(ret)
 }
